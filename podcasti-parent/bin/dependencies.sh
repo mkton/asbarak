b294165bc6337cc30f9ff4e92e4f8fe337fcb956
mkton <0.mkton.0@gmail.com>
2010-08-25 17:34:59 +0200

    facilities for download dependencies and update binaries

---
+++ podcasti-parent/bin/dependencies.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+#Dowload dependencies for Podcasti
+wget http://mirrors.ibiblio.org/pub/mirrors/maven2/net/java/dev/rome/rome/1.0.0/rome-1.0.0.jar
+wget http://mirrors.ibiblio.org/pub/mirrors/maven2/org/apache/commons/commons-io/1.3.2/commons-io-1.3.2.jar
---
+++ podcasti-parent/bin/update.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+#update logic and web-gui
+cp ../logic/target/logic-0.0.2-SNAPSHOT.jar .
+cp ../web-gui/target/web-gui-0.0.2-SNAPSHOT.war .
